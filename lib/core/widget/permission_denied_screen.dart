917513ccc981c6d896c724f1fe8cc3d783f8fa20
nimafarzin-pr <nimnim4006@Gmail.com>
2024-05-10 22:22:06 +0330

    feat: Implement create markers, permission, ang gps with riverpod

--- lib/core/widget/permission_denied_screen.dart
+++ lib/core/widget/permission_denied_screen.dart
@@ -1,40 +1,54 @@
+import 'dart:developer';
+
+import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:locate_me/core/widget/dialogs/permission.dart';
 
+import '../../features/map/provider/permission_provider.dart';
+
 class PermissionDeniedScreen extends StatelessWidget {
-  final Function onRecall;
-  const PermissionDeniedScreen({
-    super.key,
-    required this.onRecall,
-  });
+  const PermissionDeniedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Center(
-            child: Column(
+    return Consumer(
+      builder: (context, ref, child) {
+        ref.watch(permissionProvider);
+        return Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
-                'Please enable location and grant location permission to continue!'),
-            TextButton(
-                onPressed: () async {
-                  final result = await Geolocator.checkPermission();
-                  if (result == LocationPermission.always ||
-                      result == LocationPermission.whileInUse) {
-                    onRecall();
-                  } else {
-                    await showPermissionSettingsDialog(context);
-                  }
-                },
-                child: const Text("Accept"))
+            Center(
+                child: Column(
+              children: [
+                const Text(
+                    'Please enable location and grant location permission to continue!'),
+                TextButton(
+                    onPressed: () async {
+                      final permissionStatus =
+                          await Geolocator.checkPermission();
+                      if (permissionStatus ==
+                              LocationPermission.deniedForever ||
+                          permissionStatus == LocationPermission.denied) {
+                        await showPermissionSettingsDialog(context,
+                            onAcceptPressed: () async {
+                          log('??????  $permissionStatus');
+                          await AppSettings.openAppSettings(
+                              type: AppSettingsType.location);
+                        });
+                      } else {
+                        ref.refresh(permissionProvider);
+                      }
+                    },
+                    child: const Text("Accept / Refresh"))
+              ],
+            )),
           ],
-        )),
-      ],
+        );
+      },
     );
   }
 }
