451fcbdf3123eb549aa4dbd3063e7e97271217dc
nimafarzin-pr <nimnim4006@Gmail.com>
2024-08-23 11:22:18 +0330

    fix: back button color

---
+++ lib/core/widget/custom_back_icon.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../features/setting/provider/language_notifier_provider.dart';
+
+class CustomBackIcon extends StatelessWidget {
+  const CustomBackIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        return Center(
+          child: FaIcon(
+            ref.watch(languageNotifierProvider).asData?.value == 'fa'
+                ? FontAwesomeIcons.chevronRight
+                : FontAwesomeIcons.chevronLeft,
+            size: 20,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        );
+      },
+    );
+  }
+}
