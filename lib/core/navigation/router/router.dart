0991ccb852b0bb18ceefb1811a45f23d95f5496f
nimafarzin-pr <nimnim4006@Gmail.com>
2024-10-31 19:07:43 +0330

    release: version 1.0.0

--- lib/core/navigation/router/router.dart
+++ lib/core/navigation/router/router.dart
@@ -1,8 +1,5 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:locate_me/features/login/view/screen/sign_up.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:locate_me/core/navigation/routes.dart';
 import 'package:locate_me/core/widget/background_wrapper.dart';
@@ -13,7 +10,6 @@ import 'package:locate_me/features/setting/view/widgets/items/category/category_
 import '../../../features/add/view/screen/add_tab.dart';
 import '../../../features/home/view/widgets/details_view.dart';
 import '../../../features/home/view/widgets/edit/edit_view.dart';
-import '../../../features/login/view/screen/sign_in.dart';
 
 // Private navigators
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -28,46 +24,10 @@ bool isUserLoggedIn() {
 }
 
 final router = GoRouter(
-  initialLocation: Routes.signInRouteForNavigator,
+  initialLocation: Routes.root,
   navigatorKey: _rootNavigatorKey,
   debugLogDiagnostics: true,
-  // Redirect based on authentication status
-  redirect: (context, state) {
-    log('_+_+_+ ${state.matchedLocation}');
-    final loggedIn = Supabase.instance.client.auth.currentSession != null;
-    final loggingIn = state.matchedLocation == Routes.signIn;
-
-    // If the user is not logged in and not on the login page, redirect to login
-    if (!loggedIn && !loggingIn) {
-      return Routes.signInRouteForNavigator;
-    }
-
-    // If the user is logged in, prevent access to the login page
-    if (loggedIn && loggingIn) {
-      return Routes.root; // or any authenticated page like '/home'
-    }
-
-    return null; // no redirection needed
-  },
   routes: [
-    GoRoute(
-      path: Routes.signInRouteForNavigator,
-      name: Routes.signInRouteForNavigator,
-      pageBuilder: (context, state) => NoTransitionPage(
-        key: state.pageKey,
-        child: const BackgroundWrapper(
-            child: SignInScreen()), // your sign-in screen
-      ),
-    ),
-
-    GoRoute(
-      path: Routes.signUpRouteForNavigator,
-      name: Routes.signUpRouteForNavigator,
-      pageBuilder: (context, state) => NoTransitionPage(
-        key: state.pageKey,
-        child: const BackgroundWrapper(child: SignUpScreen()),
-      ),
-    ),
     // The rest of your routes with Shell Navigation
     StatefulShellRoute.indexedStack(
       builder: (context, state, navigationShell) {
